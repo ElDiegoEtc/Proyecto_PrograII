bfb7928070dc681fd2499f44d4aee0ee6b5bc3aa
Bianca <bseminario2023@udec.cl>
2023-12-09 13:45:30 -0500

    java doc para las flechas

--- logica/Flecha.java
+++ logica/Flecha.java
@@ -1,10 +1,13 @@
 package logica;
 
+/**
+ * Enumeración que define diferentes tipos de flechas con sus configuraciones predeterminadas.
+ */
 public enum Flecha {
 
     SIMPLE(false, true),  // Triangular: false, Relleno: true
     INHERITANCE(false, true),  // Triangular: false, Relleno: true
-    ASSOCIATION(true, true),  // Triangular: true(es  rombo), Relleno: true
+    ASSOCIATION(true, true),  // Triangular: true (es rombo), Relleno: true
     AGGREGATION(true, false),  // Triangular: true, Relleno: false
     COMPOSITION(false, true);  // Triangular: false, Relleno: true
 
@@ -15,10 +18,21 @@ public enum Flecha {
         this.tipoPuntaPredeterminado = tipoPuntaPredeterminado;
         this.rellenoPredeterminado = rellenoPredeterminado;
     }
+
+    /**
+     * Obtiene el valor predeterminado del tipo de punta.
+     *
+     * @return true si el tipo de punta es triangular, false si es un rombo.
+     */
     public boolean getTipoPuntaPredeterminado() {
         return tipoPuntaPredeterminado;
     }
 
+    /**
+     * Obtiene el valor predeterminado del relleno.
+     *
+     * @return true si se debe tener relleno, false de lo contrario.
+     */
     public boolean getRellenoPredeterminado() {
         return rellenoPredeterminado;
     }
