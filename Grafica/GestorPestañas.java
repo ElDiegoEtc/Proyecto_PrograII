7ea8ec707f32eb36347a3544cd8e1e793f0096ce
Diego Oyarzo <diegoyarzonavia@gmail.com>
2023-12-09 18:16:25 -0300

    Creacion logica clase GestorPestañas, que se encargara de crear pizarras

--- Grafica/GestorPestañas.java
+++ Grafica/GestorPestañas.java
@@ -1,8 +1,34 @@
 package Grafica;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GestorPestañas extends JFrame {
     private JTabbedPane tabbedPane;
 
+    public GestorPestañas() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Editor de Diagrama UML");
+
+        tabbedPane = new JTabbedPane();
+        getContentPane().add(tabbedPane, BorderLayout.CENTER);
+
+        JButton addButton = new JButton("+ Nueva Pestaña");
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                PizarraUML nuevaPizarra = new PizarraUML();
+                tabbedPane.addTab("Pestaña " + (tabbedPane.getTabCount() + 1), nuevaPizarra);
+            }
+        });
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(addButton);
+        getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+
+        pack();
+        setLocationRelativeTo(null);
+    }
 }
--- logica/main.java
+++ logica/main.java
@@ -3,6 +3,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 
+import Grafica.GestorPestañas;
 import Grafica.PizarraUML;
 import logica.command.CommandConfiguracion;
 import logica.command.CommandGuardarArchivoPizarra;
@@ -10,12 +11,12 @@ import logica.command.CommandGuardarArchivoPizarra;
 import javax.swing.*;
 
 public class main implements Serializable {
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
-            @Override
             public void run() {
-                PizarraUML editorUML = new PizarraUML();
-                editorUML.setVisible(true);
+                GestorPestañas gp = new GestorPestañas();
+                gp.setVisible(true);
             }
         });
     }
